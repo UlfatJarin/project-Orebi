1012620616845bba2f1823342b3b39736c65ded8
UlfatJarin <richiulfat@gmail.com>
2024-10-04 23:06:25 +0600

    orebi4th

--- src/component/layer/CheckBox.jsx
+++ src/component/layer/CheckBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const CheckBox = ({type ,className ,name}) => {
+const CheckBox = ({type ,className ,name ,value10 ,onChange}) => {
   return (
     <div>
-         <input className={`relative appearance-none border border-[#767676] w-[12px] h-[12px]  checked:after:content[""] checked:after:absolute after:h-[7px] after:w-[7px] checked:after:bg-[#767676] after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 ${className}`} name={name} type={type} />
+         <input className={`relative appearance-none border border-[#767676] w-[12px] h-[12px]  checked:after:content[""] checked:after:absolute after:h-[7px] after:w-[7px] checked:after:bg-[#767676] after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 ${className}`} name={name} type={type} value={value10} onChange={onChange}/>
     </div>
   )
 }
--- src/page/SignIn.jsx
+++ src/page/SignIn.jsx
@@ -12,73 +12,34 @@ const SignIn = () => {
     const District = ["Bandarban", "Barguna", "Barisal", "B Brahmanbaria", "Brahmanbaria", "Chandpur", "Chandpur", "Chattogram", "Chuadanga", "Cox's Bazar", "Dhaka", "Dinajpur", "Faridpur", "Feni", "Gaibandha", "Gazipur", "Gopalganj", "Habiganj", "Halda", "Jamalpur", "Jashore", "Jatiyo", "Jhalokati", "Jhenaidah", "Joypurhat", "Khagrachari", "Khulna", "Kishoreganj", "Kurigram", "Kushtia", "Lalmonirhat", "Madaripur", "Magura", "Manikganj", "Meherpur", "Moulvibazar", "Mymensingh", "Naogaon", "Narail", "Narayanganj", "Narsingdi", "Narsingdi", "Natore", "Netrokona", "Nilphamari", "Noakhali", "Pabna", "Panchagarh", "Patuakhali", "Patuakhali", "Pirojpur", "Rajbari", "Rajshahi", "Rangamati", "Rangpur", "Satkhira", "Shariatpur", "Sherpur", "Sirajganj", "Sunamganj", "Sylhet", "Tangail", "Thakurgaon"];
 
 
-    let [fName, setfName] = useState('');
-    let [fNameErr, setfNameErr] = useState('');
+    const [SignInInfo , setSignInInfo] =useState({
+        firstname:"",
+        lastname:"",
+        email:"",
+        number:"",
+        address1:"",
+        address2:"",
+        city:"",
+        post:"",
+        division:"",
+        district:"",
+        Password:"",
+        Repeatpassword:"",
+        agree: false
 
-    let [lName, setlName] = useState('');
-    let [lNameErr, setlNameErr] = useState('');
+    });
+    
+    console.log(SignInInfo);
 
-    let [email, setemail] = useState('');
-    let [emailErr, setemailErr] = useState('');
-
-    let [number, setnumber] = useState('');
-    let [numberErr, setnumberErr] = useState('');
-
-    let [password, setpassword] = useState('');
-    let [passwordErr, setpasswordErr] = useState('');
-
-    let [repeatpass, setrepeatpass] = useState('');
-    let [repeatpassErr, setrepeatpassErr] = useState('');
-
-    let managefName = (element)=>{
-        setfName(element.target.value);
-        setfNameErr('')
+    const OnChangeSignUp  =(event)=>{
+        const {id, value , checked} = event.target;
+        setSignInInfo({
+            ...SignInInfo,
+            [id]: id === "agree" ? checked : value,
+        })
+        
     }
-
-    let managelName = (element)=>{
-        setlName(element.target.value);
-        setlNameErr('')
-    }
-
-    let manageemail = (element)=>{
-        setemail(element.target.value);
-        setemailErr('')
-    }
-
-    let managenumber= (element)=>{
-        setnumber(element.target.value);
-        setnumberErr('')
-    }
-
-    let managepassword= (element)=>{
-        setpassword(element.target.value);
-        setpasswordErr('')
-    }
-    let managerepeatpass= (element)=>{
-        setrepeatpass(element.target.value);
-        setrepeatpassErr('')
-    }
-
-    let manageSubmit = ()=>{
-        if(!fName){
-            setfNameErr('First Name is requred');
-        }
-        if(!lName){
-            setlNameErr('Last Name is requred');
-        }
-        if(!email){
-            setemailErr('Email is requred');
-        }
-        if(!number){
-            setnumberErr('Number is requred');
-        }
-        if(!password){
-            setpasswordErr('Password is requred');
-        }
-        if(!repeatpass){
-            setrepeatpassErr('Repeat Password is requred');
-        }
-    }
+    
 
 
     return (
@@ -93,32 +54,32 @@ const SignIn = () => {
                         <div className='border-b border-[#F0F0F0]'>
                             <Tittle title='Your Personal Details' />
                             <div className='flex flex-wrap gap-x-10 gap-y-6 mb-[69px]'>
-                                <Input valuei={fName} onChange={managefName} id='firstname' type='text' placeholder='First Name' LabelText='First Name' >
-                                <p className='absolute top-full font-DM text-red-500 text-[10px] '>{fNameErr}</p>
+                                <Input valuei={SignInInfo.firstname}  id='firstname' type='text' placeholder='First Name' LabelText='First Name' onChange={OnChangeSignUp} >
+                                {/* <p className='absolute top-full font-DM text-red-500 text-[10px] '>{fNameErr}</p> */}
                                 </Input>
 
-                                <Input valuei={lName} onChange={managelName} id='lastname' type='text' placeholder='Last Name' LabelText='Last Name'>
-                                <p className='absolute top-full font-DM text-red-500 text-[10px] '>{lNameErr}</p>
+                                <Input valuei={SignInInfo.lastname}  id='lastname' type='text' placeholder='Last Name' LabelText='Last Name' onChange={OnChangeSignUp}>
+                                {/* <p className='absolute top-full font-DM text-red-500 text-[10px] '>{lNameErr}</p> */}
                                 </Input>
-                                <Input valuei={email} onChange={manageemail} id='email' type='email' placeholder='company@domain.com' LabelText='Email address' >
-                                <p className='absolute top-full font-DM text-red-500 text-[10px] '>{emailErr}</p>
+                                <Input valuei={SignInInfo.email}  id='email' type='email' placeholder='company@domain.com' LabelText='Email address' onChange={OnChangeSignUp} >
+                                {/* <p className='absolute top-full font-DM text-red-500 text-[10px] '>{emailErr}</p> */}
                                 </Input>
-                                <Input  valuei={number} onChange={managenumber} id='number' type='number' placeholder='Your phone number' LabelText='Telephone' >
-                                <p className='absolute top-full font-DM text-red-500 text-[10px] '>{numberErr}</p>
+                                <Input  valuei={SignInInfo.number}  id='number' type='number' placeholder='Your phone number' LabelText='Telephone' onChange={OnChangeSignUp} >
+                                {/* <p className='absolute top-full font-DM text-red-500 text-[10px] '>{numberErr}</p> */}
                                 </Input>
                             </div>
                         </div>
                         <div className=' border-b border-[#F0F0F0]'>
                             <Tittle title='New Customer' />
                             <div className='flex flex-wrap gap-x-10 gap-y-6 mb-[69px]'>
-                                <Input id='address1' type='text' LabelText='Address 1 ' placeholder='4279 Zboncak Port Suite 6212' />
-                                <Input id='address2' type='text' LabelText='Address 2 ' placeholder='—' />
-                                <Input id='city' type='text' LabelText='City ' placeholder='Your city' />
-                                <Input id='post' type='text' LabelText='Post Code' placeholder='05228' />
+                                <Input id='address1' valuei={SignInInfo.address1} type='text' LabelText='Address 1 ' placeholder='4279 Zboncak Port Suite 6212' onChange={OnChangeSignUp}/>
+                                <Input id='address2' valuei={SignInInfo.address2} type='text' LabelText='Address 2 ' placeholder='—'  onChange={OnChangeSignUp}/>
+                                <Input id='city'     valuei={SignInInfo.city}     type='text' LabelText='City '      placeholder='Your city' onChange={OnChangeSignUp} />
+                                <Input id='post'     valuei={SignInInfo.post}     type='text' LabelText='Post Code'  placeholder='05228' onChange={OnChangeSignUp} />
                                 <div className=' w-[507px] pb-2 border-b border-[#F0F0F0] flex flex-col'>
                                     <label className='font-DM font-bold leading-6 py-1 text-textcolor' htmlFor="Division">Division</label>
-                                    <select className='bg-transparent' name="" id="Division">
-                                        <option className='font-DM text-sm text-textcolor2 py-2 outline-none capitalize' value="Please select">Please select</option>
+                                    <select className='bg-transparent' name="" id="division" value={SignInInfo.division} onChange={OnChangeSignUp}>
+                                        <option className='font-DM text-sm text-textcolor2 py-2 outline-none capitalize' value="Please select"></option>
                                         {
                                             Division.map((item, index) => (
                                                 <option className='font-DM text-sm text-textcolor2 py-2 outline-none capitalize ' key={index} value={item}>{item}</option>
@@ -128,8 +89,8 @@ const SignIn = () => {
                                 </div>
                                 <div className=' w-[507px] pb-2 border-b border-[#F0F0F0] flex flex-col bg-transparent'>
                                     <label className='font-DM font-bold leading-6 py-1 text-textcolor' htmlFor="District">District</label>
-                                    <select className='bg-transparent' name="" id="Division">
-                                        <option className='font-DM text-sm text-textcolor2 py-2 outline-none capitalize bg-none' value="Please select">Please select</option>
+                                    <select className='bg-transparent' name="" id="district" value={SignInInfo.district} onChange={OnChangeSignUp}>
+                                        <option className='font-DM text-sm text-textcolor2 py-2 outline-none capitalize bg-none' value="Please select"></option>
                                         {
                                             District.map((item, index) => (
                                                 <option className='font-DM text-sm text-textcolor2 py-2 outline-none capitalize bg-none' key={index} value={item}>{item}</option>
@@ -143,18 +104,18 @@ const SignIn = () => {
                         <div className=' border-b border-[#F0F0F0]'>
                             <Tittle title='Your Password' />
                             <div className='flex flex-wrap gap-x-10 gap-y-6 mb-[69px]'>
-                                <Input valuei={password} onChange={managepassword} id='Password' type='password' LabelText=' Password' placeholder='Password' >
-                                <p className='absolute top-full font-DM text-red-500 text-[10px] '>{passwordErr}</p>
+                                <Input valuei={SignInInfo.Password}  id='Password' type='password' LabelText=' Password' placeholder='Password' onChange={OnChangeSignUp}>
+                                {/* <p className='absolute top-full font-DM text-red-500 text-[10px] '>{passwordErr}</p> */}
                                 </Input>
-                                <Input  valuei={repeatpass} onChange={managerepeatpass} id='Repeat password' type='password' LabelText='Repeat Password ' placeholder='Repeat password' >
-                                <p className='absolute top-full font-DM text-red-500 text-[10px] '>{repeatpassErr}</p>
+                                <Input  valuei={SignInInfo.Repeatpassword}  id='Repeatpassword' type='password' LabelText='Repeat Password ' placeholder='Repeat password' onChange={OnChangeSignUp} >
+                                {/* <p className='absolute top-full font-DM text-red-500 text-[10px] '>{repeatpassErr}</p> */}
                                 </Input>
                             </div>
                         </div>
 
                         <div className='flex flex-col gap-6'>
                             <div className='flex gap-4 items-center'>
-                                <CheckBox type='checkbox' />
+                                <CheckBox type='checkbox' id='agree' value10={SignInInfo.agree} onChange={OnChangeSignUp} />
                                 <p className='font-DM text-sm text-textcolor2'>I have read and agree to the Privacy Policy</p>
                             </div>
                             <div  className='flex gap-4 items-center'>
@@ -162,9 +123,9 @@ const SignIn = () => {
                                 <CheckBox type='radio' name='subscribe' />
                                 <p className='font-DM text-sm text-textcolor2 mr-3'>Yes</p>
                                 <CheckBox type='radio' name='subscribe' />
-                                <p className='font-DM text-sm text-textcolor2 '>No</p>
+                                <p className='font-DM text-sm text-textcolor2'>No</p>
                             </div>
-                            <Button onClick={manageSubmit} className='max-w-[200px]' btntext='Sign up'/>
+                            <Button  className='max-w-[200px]' btntext='Sign up'/>
 
                         </div>
                     </div>
